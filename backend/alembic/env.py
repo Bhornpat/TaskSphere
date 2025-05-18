72ab63d334ec21d68f5bde8efaf25297fb61f780
Bhornpat <thebowiiz2543@gmail.com>
2025-05-18 17:18:19 +0700

    Updated all

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -2,6 +2,8 @@ import sys, os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from dotenv import load_dotenv
+load_dotenv()  # this loads .env and makes DATABASE_URL visible
 
 from models import Base
 from database import engine
@@ -11,8 +13,10 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+# Inject into config for %(DATABASE_URL)s to work
 from alembic import context
-
+if os.getenv("DATABASE_URL"):
+    context.config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
