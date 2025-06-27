12fc01753d315a5a73c60092845df529f5d36c75
Bhornpat <thebowiiz2543@gmail.com>
2025-06-27 19:24:58 +0700

    edited cors

--- backend/main.py
+++ backend/main.py
@@ -43,6 +43,7 @@ origins = [
     "https://tasksphere.tomo-naja.org",
     "https://tasksphere-flame.vercel.app",
     "https://tasksphere-production-06ff.up.railway.app",
+    "https://tasksphere-production-85ff.up.railway.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://vmserver:3000",
